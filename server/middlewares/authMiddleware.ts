c8d52d16148df1a2efbeff577807a52775642732
Yashvardhan Singh <yash2212003@gmail.com>
2023-11-09 23:50:12 +0530

    bugs fixed

--- server/middlewares/authMiddleware.ts
+++ server/middlewares/authMiddleware.ts
@@ -1,29 +1,32 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import User from "../models/userModel";
 import asyncHandler from "express-async-handler";
-import { Request ,Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from "express";
 
-interface CustomRequest extends Request{
-    user?:string
+interface CustomRequest extends Request {
+  user?: string;
 }
-export const protect = asyncHandler(async(req:CustomRequest,res:Response,next:NextFunction) => {
-    let token: string | undefined;
-    let decoded: { id: string } | undefined;
 
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-        try {
-            token = req.headers.authorization.split(" ")[1];
-            if(process.env.JWT_SECRET){
-            decoded = jwt.verify(token,process.env.JWT_SECRET) as {id:string};
-            }
-            if(decoded){
-                 req.user = (await User.findById(decoded.id).select("-password"))?.id;
-            }
-            next();
-        } catch (error) {
-            res.status(400);
-            throw new Error("Not Authorized..Failed");
-        }
+export const protect = asyncHandler(async (req: CustomRequest, res: Response, next: NextFunction) => {
+  let token: string | undefined;
+  let decoded: JwtPayload | string = '';
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      if (process.env.JWT_SECRET_KEY) {
+        decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      }
+      if (typeof decoded === 'string') {
+        res.status(400);
+        throw new Error("Not Authorized..Failed!!!");
+      } else {
+        req.body.user = await User.findById(decoded.id).select("-password");
+        next();
+      }
+    } catch (error) {
+      res.status(400);
+      throw new Error("Not Authorized..Failed");
     }
-  
-})
+  }
+});
